5890a6fb99d86b088f43154284362ebb24053c1a
Konstantin Sabanin <sabanin_k@mail.ru>
2022-06-10 16:22:54 +0300

    Add logo and icons to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,33 +1,43 @@
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.scss'
+import { ReactComponent as SearchIcon } from '../../assets/svg/search.svg'
+import { ReactComponent as CartIcon } from '../../assets/svg/cart.svg'
 
 export const Header: FC = () => {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                <nav>
-                    <ul className={styles.navList}>
+                <nav className={styles.nav}>
+                    <ul className={`${styles.desctopList} ${styles.list}`}>
                         <li>
-                            <Link to='/' className={styles.navItem}>
+                            <Link to='/' className={styles.link}>
                                 Каталог товаров
                             </Link>
                         </li>
                         <li>
-                            <Link to='/' className={styles.navItem}>
+                            <Link to='/' className={styles.link}>
                                 Вязание на заказ
                             </Link>
                         </li>
-                        {/* <li>
-                            <Link to='/' className={`${styles.navItem} ${styles.navDesktopItem}`}>
-                                Новые поступления
+                        <li>
+                            <Link to='/' className={styles.link} >
+                                <div className={styles.logo}></div>
+                            </Link>
+                        </li>
+                    </ul>
+                    <ul className={styles.list}>
+                        <li>
+                            <Link to='/' className={styles.link}>
+                                <SearchIcon className={styles.searchIcon} />
                             </Link>
                         </li>
                         <li>
-                            <Link to='/' className={`${styles.navItem} ${styles.navDesktopItem}`}>
-                                Распродажа
+                            <Link to='/' className={styles.link}>
+                                <CartIcon />
+                                Корзина: пока пуста
                             </Link>
-                        </li> */}
+                        </li>
                     </ul>
                 </nav>
 
