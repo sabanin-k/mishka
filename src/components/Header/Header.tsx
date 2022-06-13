6c4886a844f409cd4f36b6f1b0f249c0fce162b8
Konstantin Sabanin <sabanin_k@mail.ru>
2022-06-13 20:31:27 +0300

    Header almost done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,18 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.scss'
 
 export const Header: FC = () => {
+    const [isActiveMenu, setActiveMenu] = useState(false)
+    const handleSetMenuActive = () => {
+        setActiveMenu(!isActiveMenu)
+    }
+
     return (
         <header className={styles.header}>
             <div className={styles.container}>
                 <nav className={styles.nav}>
-                    <ul className={styles.leftNav}>
+                    <ul className={`${styles.leftNav} ${isActiveMenu && styles.visuallyHidden}`}>
                         <li className={styles.catalog}>
                             <Link to='/' className={styles.link}>
                                 <span className={styles.span}>Каталог товаров</span>
@@ -19,28 +24,41 @@ export const Header: FC = () => {
                             </Link>
                         </li>
                     </ul>
+                    <div className={styles.desktopOnly}>
+                        <Link to='/' className={styles.link}>
+                            <span className={styles.span}>Новые поступления</span>
+                        </Link>
+                        <Link to='/' className={styles.link}>
+                            <span className={styles.span}>Распродажа</span>
+                        </Link>
+                    </div>
                     <div className={styles.logoContainer}>
                         <Link to='/' className={styles.link} >
                             <div className={styles.logo}></div>
                         </Link>
                     </div>
-                    <ul className={styles.rightNav}>
+                    <ul className={`${styles.rightNav} ${isActiveMenu && styles.visuallyHidden}`}>
                         <li>
                             <Link to='/' className={styles.link}>
-                                <div className={styles.searchIcon}></div>
-                                <span className={styles.searchSpan}>Поиск по сайту</span>
+                                <div className={styles.search}>
+                                    <div className={styles.searchIcon}></div>
+                                    <span className={`${styles.span} ${styles.searchSpan}`}>Поиск по сайту</span>
+                                </div>
                             </Link>
                         </li>
                         <li>
                             <Link to='/' className={styles.link}>
                                 <div className={styles.cart}>
                                     <div className={styles.cartIcon}></div>
-                                    <span className={styles.cartSpan}>Корзина: пока пуста</span>
+                                    <span className={`${styles.span} ${styles.cartSpan}`}>Корзина: пока пуста</span>
                                 </div>
                             </Link>
                         </li>
                     </ul>
-                    <button className={styles.burgerMenu}></button>
+                    <div className={styles.delivery}>
+                        <span>Бесплатная доставка по России</span>
+                    </div>
+                    <button onClick={handleSetMenuActive} className={isActiveMenu ? styles.burgerMenu : styles.crossMenu}></button>
                 </nav>
             </div>
         </header>
