fe5cc50245fb31255b7780732eacba6d626b6ba3
Konstantin Sabanin <sabanin_k@mail.ru>
2022-07-01 17:00:15 +0300

    Add Video plug in Catalog layout

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,19 +1,24 @@
 import { FC } from 'react'
-import { useMatchMedia } from '../../hooks/useMatchMedia'
-import { ProductCard } from './ProductCard'
 import desktopImage1 from '../../assets/jpg/photo-1-desktop.jpg'
-import desktopImage2 from '../../assets/jpg/photo-2-desktop.jpg'
-import desktopImage3 from '../../assets/jpg/photo-3-desktop.jpg'
-import tabletImage1 from '../../assets/jpg/photo-1-tablet.jpg'
-import tabletImage2 from '../../assets/jpg/photo-2-tablet.jpg'
-import tabletImage3 from '../../assets/jpg/photo-3-tablet.jpg'
 import mobileImage1 from '../../assets/jpg/photo-1-mobile.jpg'
+import tabletImage1 from '../../assets/jpg/photo-1-tablet.jpg'
+import desktopImage2 from '../../assets/jpg/photo-2-desktop.jpg'
 import mobileImage2 from '../../assets/jpg/photo-2-mobile.jpg'
+import tabletImage2 from '../../assets/jpg/photo-2-tablet.jpg'
+import desktopImage3 from '../../assets/jpg/photo-3-desktop.jpg'
 import mobileImage3 from '../../assets/jpg/photo-3-mobile.jpg'
+import tabletImage3 from '../../assets/jpg/photo-3-tablet.jpg'
+import { useMatchMedia } from '../../hooks/useMatchMedia'
+import { Button } from '../common/Button'
+import { ProductCard } from './ProductCard'
 import styles from './Catalog.module.scss'
 
 export const Catalog: FC = () => {
     const { isDesktop, isTablet, isMobile } = useMatchMedia()
+
+    const handleClick = () => {
+        console.log('Сделать заказ');        
+    }
 
     return (
         <section>
@@ -44,8 +49,24 @@ export const Catalog: FC = () => {
                         price={1500}
                     />
                 </div>
-                <div>
-                    
+                <div className={styles.videoWrapper}>
+                    <div className={styles.videoImg}></div>
+                    <div className={styles.videoDesc}>
+                        <div>
+                            <div className={styles.cameraIcon} />
+                            <h3 className={styles.h3}>Процесс производства</h3>
+                        </div>
+                        <div>
+                            <p>
+                                По просьбам наших любимых фолловеров
+                                мы сняли для вас подробное видео о том,
+                                как появляются наши товары.
+                            </p>
+                        </div>
+                        <div className={styles.button}>
+                            <Button text='Сделать заказ' handleClick={handleClick} />
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
